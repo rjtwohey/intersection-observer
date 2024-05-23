71ed56d982a24ca2c576a850fa5172c2663f29c8
rtwohey <rtwohey@ionic.io>
2024-05-23 15:56:19 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'intersection-observer',
+  webDir: 'dist'
+};
+
+export default config;
